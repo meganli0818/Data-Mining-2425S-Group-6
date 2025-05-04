7d3547d25494a062e69c9de7a13bdf50f1bfe970
amkim26 <151073332+amkim26@users.noreply.github.com>
2025-05-04 03:51:48 -0400

    Updated edge_merge to fit new edge ullman's
    this aint workin thoXDDDDDDDDDDD

--- edge_merge_test.py
+++ edge_merge_test.py
@@ -1,6 +1,7 @@
 import networkx as nx
-from ullman_algo import UllmanAlgorithm
+from ullman_algo_edge import UllmanAlgorithmEdge
 from collections import defaultdict
+
 
 def edge_based_merge(G, P):
     """
@@ -27,71 +28,69 @@ def edge_based_merge(G, P):
 
     merged_results = []
 
-    # Loop through all possible k-1 subgraphs and check for isomorphism
+    # Loop through all edges in P, removing one at a time.
     for u_p, v_p in P.edges():
         P_rem = nx.Graph(P)
-        P_rem.remove_edge(u_p, v_p)
+        P_rem.remove_edge(u_p,v_p)
+        iso = UllmanAlgorithmEdge(G, P_rem)
 
-        # Avoid checking isomorphisms for 0-degree nodes
-        for node in (u_p, v_p):
-            if P_rem.degree(node) == 0:
-                P_rem.remove_node(node)
-    
-        # Try removing this P-edge against each G-edge
-        for u_g, v_g in G.edges():
+        # Check if the remaining "root" size k-1 P-graph is a subgraph of G.
+        # If it is, we can merge the two graphs.
+        if iso.ullman(False):
+            unmapped_edges_g = iso.get_unmapped_edges_in_G()
             G_rem = nx.Graph(G)
-            G_rem.remove_edge(u_g, v_g)
+            for unmapped_edge_g in unmapped_edges_g:
+                G_rem.remove_edge(*unmapped_edge_g)
+            print((unmapped_edges_g))
+            exact_match = UllmanAlgorithmEdge(G_rem, P_rem)
+            print("G nodes:", list(G_rem.nodes(data=True)))
+            print("G edges:", list(G_rem.edges(data=True)))
+            print("P nodes:", list(P_rem.nodes(data=True)))
+            print("P edges:", list(P_rem.edges(data=True)))
+            if exact_match.ullman(True):
+                unmapped_p_nodes = iso.get_unmapped_vertices_in_P()
+                unmapped_g_nodes = iso.get_unmapped_vertices_in_G()
+                
+                unmapped_edges_p = iso.get_unmapped_edges_in_P()
 
-            for node in (u_p, v_p): 
-                if G_rem.degree(node) == 0: # Avoid checking isomorphisms for 0-degree nodes
-                    G_rem.remove_node(node)
+                # /----- Create new merged graph using G as base -----/
+                merged_graph = nx.Graph(G)
 
-            # Check if the remaining "root" size k-1 graph is a subgraph of G.
-            # If it is, we can merge the two graphs.
-            iso = UllmanAlgorithm(G_rem, P_rem)
-            if not iso.ullman(False):
-                continue
-            mapping = iso.get_mapping()
+                # The case where P (to be added to G) does not have an unmapped node
+                # // Weird nx behavior about implicitly adding any nodes that don’t already exist when defining edges
+                if(len(unmapped_p_nodes) == 0):
+                    for unmapped_edge_p in unmapped_edges_p:
+                        merged_graph.add_edge(*unmapped_edge_p)
+                        merged_results.append(merged_graph)
 
-            # We want u_p to be the "anchor"  (degree > 1), 
-            # and v_p to be the "leaf" (degree == 1)
-            # This is for consistency, we need to know which node to refer to
-            if G.degree(u_g) >= G.degree(v_g):
-                g_leaf = v_g
-            else:
-                g_leaf = u_g
+                # Using vertex mapping dictionary:
+                # 1. Find mapping of u, v to G
+                # 2. If these do not map to anything in G, we create new nodes.
+                # 3. Then, we add define the edge between new node and merged graph.
+                for unmapped_edge_p in unmapped_edges_p:
+                    u, v = unmapped_edge_p
+                    mapping = iso.get_mapping()
 
-            if P.degree(u_p) >= P.degree(v_p):
-                p_leaf = v_p
-                p_anchor = u_p
-            else:
-                p_leaf = u_p
-                p_anchor = v_p
-            
-            p_leaf_label = P.nodes[p_leaf].get('label')
-            g_leaf_label = G.nodes[g_leaf].get('label')
-
-            
-            # /-----Candidate 1-----/
-            # attach the P-leaf (node) along with its edge itself to G
-            cand1 = nx.Graph(G)
-            p_node = max(G.nodes()) + 1
-            # hook it up to the mapped anchor
-            cand1.add_node(p_node, label=p_leaf_label)
-            cand1.add_edge(mapping[p_anchor], p_node)
-            merged_results.append(cand1)
-
-            # /-----Candidate 2-----/
-            # add back the join edge between mapped nodes (only if labels match)
-            # get the two labels
-    
-            if p_leaf_label == g_leaf_label:
-                cand2 = nx.Graph(G)
-                cand2.add_edge(mapping[p_anchor], p_leaf)
-                merged_results.append(cand2)
-
-            return merged_results
+                    if any(node not in mapping for node in (u, v)): 
+                        # /--- Candidate 1 ---/
+                        # If P_node is missing in G, create node and add to merged graph
+                        for unmapped_node_p in unmapped_p_nodes:
+                            new_node = unmapped_node_p
+                            merged_graph.add_node(new_node)
+                            existing_node = u if u in mapping else v
+                            merged_graph.add_edge(new_node, mapping[existing_node])
+                        
+                        # /--- Candidate 2 ---/
+                        # Check if unmapped node p and unmapped node g are equal 
+                            for unmapped_node_g in unmapped_g_nodes:
+                                if G.nodes[unmapped_node_p]['label'] == G.nodes[unmapped_node_g]['label']: 
+                                    merged_graph2 = nx.Graph(G)
+                                    merged_graph2.add_edge(unmapped_node_g, mapping[existing_node])
+                        merged_results.append(merged_graph)
+                        merged_results.append(merged_graph2)
     return merged_results
+
+
 
 def k1_join(G, P):
     """
@@ -133,92 +132,29 @@ def k1_join(G, P):
     merged_results.append(cand)
     return merged_results
 
-def node_based_merge(G, P):
-    """
-    Merges two graphs based on node-based subgraph isomorphism.
-
-    This function attempts to merge two size k graphs, `G` and `P`, by iteratively 
-    removing a node v from `P` to find a "root" graph of size k-1, and checking whether the 
-    root is a subgraph of G using Ullman's algorithm. If a match is found, it  
-    inserts v into G and connects v to all nodes in the root of G mapping to nodes that
-    v was connected to in the root of P. Finally, it generates two size k+1 merged graphs: one where v
-    is connected to the node of G not in the root, and another where v is not.
-
-    Args:
-        G (nx.Graph): The first graph to be merged.
-        P (nx.Graph): The second graph to be merged.
-
-    Returns:
-        list: The two possible merged graphs. Each merged graph is a NetworkX 
-              graph object. If no valid merges are found, an empty list 
-              is returned.
-
-    Notes:
-        - If the number of nodes in `P` does not match the number of nodes 
-          in `G`, the function returns `None`.
-    """
-    # Ensure P and G are the same size.
-    if len(P.nodes()) != len(G.nodes()):
-        return None
-    merged_results = []
-    
-    # Loop through all nodes in P, removing one at a time.
-    for node in P.nodes():
-        P_remove_node = nx.Graph(P)
-        P_remove_node.remove_node(node)
-        ullman = UllmanAlgorithm(G, P_remove_node)
-
-        # Check if the remaining "root" size k-1 graph is a subgraph of G.
-        # If it is, we can merge the two graphs.
-        if ullman.ullman(False):
-            # Get the mapping of the nodes in root of P to root of G
-            unmapped_nodes = ullman.get_unmapped_vertices()
-            G_remove_node = nx.Graph(G)
-            for unmapped_node in unmapped_nodes:
-                G_remove_node.remove_node(unmapped_node)  
-            exact_match = UllmanAlgorithm(G_remove_node, P_remove_node)
-            if exact_match.ullman(True):
-                mapping = ullman.get_mapping()
-
-                # Create a new graph by merging G and P
-                merged_graph = nx.Graph(G)
-                removed_node_neighbors = list(P.neighbors(node))
-                
-                new_node = max(G.nodes()) + 1 if G.nodes() else 1
-                merged_graph.add_node(new_node, label=P.nodes[node]['label'])
-            
-                # Connect the new node to the nodes in G that correspond to the isomorphism
-                # between P and G roots
-                for neighbor in removed_node_neighbors:
-                    merged_graph.add_edge(new_node, mapping[neighbor])
-                
-                # Connect the new node to the node in G that is not in the root for
-                # a second merged graph
-                merged_graph2 = nx.Graph(merged_graph)
-                for unmapped_node in unmapped_nodes:
-                    merged_graph2.add_edge(new_node, unmapped_node)
-                merged_results.append(merged_graph)
-                merged_results.append(merged_graph2)
-        return merged_results
 
 def main():
     G = nx.Graph()
-    G.add_node(0, label='X')
-    G.add_node(1, label='Y')
-    G.add_node(2, label='Z')
-    G.add_edges_from([(0, 1), (1,2)])
+    G.add_node(0, label='A')
+    G.add_node(1, label='A')
+    G.add_node(2, label='B')
+    G.add_node(3, label='C')
+    G.add_edges_from([(0, 1), (1,2), (0,2), (1,3)])
 
     P = nx.Graph()
-    P.add_node(0, label='X')
-    P.add_node(1, label='Y')
-    P.add_node(2, label='Z')
-    P.add_edges_from([(0, 1), (1,2)])
+    P.add_node(0, label='A')
+    P.add_node(1, label='A')
+    P.add_node(2, label='B')
+    P.add_node(3, label='C')
+    P.add_edges_from([(0, 1), (1,2), (0,2), (2,3)])
 
     print("\n--- Input Graphs ---")
     print("G nodes:", list(G.nodes(data=True)))
     print("G edges:", list(G.edges()))
     print("P nodes:", list(P.nodes(data=True)))
     print("P edges:", list(P.edges()))
+    print("END")
+    print("\n")
 
     # Run your merge
     merged = edge_based_merge(G, P)
@@ -231,5 +167,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
